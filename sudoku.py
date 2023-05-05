3bd80f937488081f62a99bb8d4168d7d0c3f7532
asaldelkhosh <asaldelkhosh@gmail.com>
2023-05-05 18:57:34 +0430

    getting all the values

--- sudoku.py
+++ sudoku.py
@@ -1,30 +1,17 @@
-import numpy as np
-
 # Get n from user, n*n is the size of our matrix
 n = input('n : ')
 n = int(n)
-np.zeros((n, n), dtype = int)
 
 # Get c from user, c is the number of value that are in matrix
 c = input('c : ')
 c = int(c)
 
-# Get values and the positions
+# Get values and the positions and changing them in arry
 while c > 0:
     i = input('i : ')
-    if i >= 0 and i <= n-1:
-        i = int(i)
-        j = input('j : ')
-    else:
-        print('Please enter a number between 0 to {}'.formt(n-1))    
-    if j >= 0 and j <= n-1:
-        j = int(j)
-        value = input('value : ')
-    else:
-        print('Please enter a number between 0 to {}'.formt(n-1))
-    if value >= 1 and value <= n:        
-        value = int(value)
-    else:
-        print('Please enter a number between 1 to {}'.formt(n))    
+    i = int(i)
+    j = input('j : ')
+    j = int(j)
+    value = input('value : ')   
+    value = int(value)
     c = c - 1
-

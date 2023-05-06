7d9bbe8f8ab3e536317a2122842a28ff09ba2dee
asaldelkhosh <asaldelkhosh@gmail.com>
2023-05-06 18:06:07 +0430

    last changes

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # Get n from user, n*n is the size of our matrix
 n = input('n : ')
 n = int(n)
@@ -17,28 +18,13 @@ while c > 0:
     i = int(i)
     j = input('j : ')
     j = int(j)
+
     value = input('value : ')
     value = int(value)
-    if i >= 0 and i <= n-1:
-        pass
-    else:
-        i = input('Please enter a number between 0 to {} for i: '.format(n-1))
-        i = int(i)
-
-    if j >= 0 and j <= n-1:
-        pass
-    else:
-        j = input('Please enter a number between 0 to {} for j: '.format(n-1)) 
-        j = int(j)
-        
-    if value >= 1 and value <= n:
-        pass
-    else:
-        value = input('Please enter a number between 1 to {} for value: '.format(n))
-        value = int(value)
 
     arry[i, j] = value
     
     c = c - 1
 
-    
+ 
+ 
